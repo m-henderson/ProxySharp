9751b9b964760d058b72bc9e1f6d5fad3074cbbd
Michael Henderson <mvhenderson.4@gmail.com>
2018-06-15 06:12:01 -0500

    Initial Commit

---
+++ Proxy.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProxySharp
+{
+    /// <summary>
+    /// Holds all of the logic for manipulating proxies. 
+    /// </summary>
+    public class Proxy
+    {
+        /// <summary>
+        /// the queue the holds all of the proxies returned from the scraper
+        /// </summary>
+        private static List<string> queue = new List<string>();
+
+        /// <summary>
+        /// the static constructor that starts the scraper and adds proxies to the queue
+        /// </summary>
+        static Proxy()
+        {
+            queue = Scrape.ScrapeProxies();
+        }
+
+        /// <summary>
+        /// Gets a list of proxies that are currently in the queue.
+        /// </summary>
+        /// <returns></returns>
+        public static List<string> GetProxies()
+        {
+            return queue;
+        }
+
+        /// <summary>
+        /// Gets the index of a proxy.
+        /// </summary>
+        /// <param name="proxy">the proxy that you want the index of</param>
+        /// <returns>return a integer that indicates the index of the given proxy</returns>
+        public static int GetIndex(string proxy)
+        {
+            return queue.IndexOf(proxy);
+        }
+
+        /// <summary>
+        /// Clears the queue and adds a fresh list of proxies to the queue.
+        /// </summary>
+        public static void RenewQueue()
+        {
+            queue.Clear();
+            queue = Scrape.ScrapeProxies();
+        }
+
+        /// <summary>
+        /// Adds the specified proxy to the queue.
+        /// </summary>
+        /// <param name="ip">The ip address of the proxy to add to the queue.</param>
+        /// <param name="port">The port number of the proxy to add to the queue.</param>
+        public static void AddProxy(string ip, string port)
+        {
+            queue.Add(ip + ":" + port);
+        }
+
+        /// <summary>
+        /// Removes the first proxy in the queue.
+        /// </summary>
+        public static void PopProxy()
+        {
+            var proxy = queue.First();
+            queue.Remove(proxy);
+        }
+
+        /// <summary>
+        /// Gets a single proxy from the queue.
+        /// </summary>
+        /// <returns>A proxy server IP and Port address.</returns>
+        public static string GetSingleProxy()
+        {
+            var proxy = queue.First();
+            return proxy;
+        }
+
+    }
+}
---
+++ Scrape.cs
@@ -0,0 +1,78 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProxySharp
+{
+    /// <summary>
+    /// Responsible for all of the logic that us used for scraping proxies. 
+    /// </summary>
+    public class Scrape
+    {
+        /// <summary>
+        /// Scrapes a list of proxies. 
+        /// </summary>
+        /// <returns>List of proxies.</returns>
+        public static List<string> ScrapeProxies()
+        {
+            string url = "https://free-proxy-list.net/";
+            HtmlWeb hw = new HtmlWeb();
+            HtmlDocument doc = hw.Load(url);
+            var proxyTable = doc.DocumentNode.SelectSingleNode("//table");
+
+            var proxyQueue = new List<string>();
+
+            var ip = ScrapeProxieIp(proxyTable);
+            var ports = ScrapeProxiePort(proxyTable);
+            var proxy = "";
+
+            foreach (var nw in ip.Zip(ports, Tuple.Create))
+            {
+                proxy = nw.Item1 + ":" + nw.Item2;
+                proxyQueue.Add(proxy);
+            }
+
+            return proxyQueue;
+        }
+
+        /// <summary>
+        /// Gets all of the Proxy Ip Addreses from the scraped proxy table.
+        /// </summary>
+        /// <param name="proxyTable">The scraped table that holds all of the proxy information.</param>
+        /// <returns>A list of proxie ip addreses.</returns>
+        private static List<string> ScrapeProxieIp(HtmlNode proxyTable)
+        {
+            var proxyIp = new List<string>();
+
+            var allPorts = proxyTable.SelectNodes("tbody/tr/td[1]");
+
+            foreach (var port in allPorts)
+            {
+                proxyIp.Add(port.InnerHtml);
+            }
+
+            return proxyIp;
+        }
+
+        /// <summary>
+        /// Gets all of the Proxy port numbers from the scraped proxy table.
+        /// </summary>
+        /// <param name="proxyTable">The scraped table that holds all of the proxy information.</param>
+        /// <returns>A list of proxie ports.</returns>
+        private static List<string> ScrapeProxiePort(HtmlNode proxyTable)
+        {
+            var proxyPorts = new List<string>();
+
+            var allPorts = proxyTable.SelectNodes("tbody/tr/td[2]");
+
+            foreach (var port in allPorts)
+            {
+                proxyPorts.Add(port.InnerHtml);
+            }
+
+            return proxyPorts;
+        }
+
+    }
+}
