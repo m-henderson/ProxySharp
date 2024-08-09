8c8199dc0a108391ddd5c2bc4bc19924d94aaef3
Miiraak <miiraak@feynlok.ch>
2024-08-09 22:25:05 +0200

    ## Enhance `Proxy` and upgrade `Scrape` classes with a new method
    ### Purpose :
    - [ Filter country ](https://github.com/m-henderson/ProxySharp/issues/6)
    
    ### Details :
    - Added `RenewFilteredProxies` method to the `Proxy` class for filtered proxies renewal.
    - Updated `ScrapeProxies` method in the `Scrape` class to accept optional filtering parameters. Country code gathering, and filtering if all conditions are met.
    - Added `ScrapingProxyCountryCode` method to the `Scrape` class to retrieve country codes.
    - Added `FilterProxy` method to the `Scrape` class for proxies filtering.
    
    ### Others changes :
    - Corrected comments in the `Proxy` class for grammar and clarity.
    - Reorganized methods in the `Proxy` class for better structure.
    
    ### Notes :
    It's a more important pull-request, so i've decided to do some tests before.
    It's better to have double check, let me know if you don't like something, or if you see something strange or wrong.
    
    What i've done :
    - The metric code seem fine for me.
    - I've set basic CodeQL analyzis, that passed,
    - I've build the package and done tests with [ProxySharpTester](https://github.com/Miiraak/ProxySharpTester) that are concluents.

--- Scrape.cs
+++ Scrape.cs
@@ -11,10 +11,12 @@ namespace ProxySharp
     public class Scrape
     {
         /// <summary>
-        /// Scrapes a list of proxies. 
+        /// Scrapes a list of proxies by country code. If the country code is not set or not find the list will not be filtered 
         /// </summary>
-        /// <returns>List of proxies.</returns>
-        public static List<string> ScrapeProxies()
+        /// <param name="countryCode">The country code choosen to filters list. (optionnal)</param>
+        /// <param name="excludeCountry">Set to true to exclude all proxies that match the filter; set to false to include only proxies that match the filter. (optionnal)</param>
+        /// <returns>A list of proxies.</returns>
+        public static List<string> ScrapeProxies(string countryCode = null, bool excludeCountry = false)
         {
             string url = "https://free-proxy-list.net/";
             HtmlWeb hw = new HtmlWeb();
@@ -25,6 +27,7 @@ namespace ProxySharp
 
             var ip = ScrapeProxieIp(proxyTable);
             var ports = ScrapeProxiePort(proxyTable);
+            var codes = ScrapingProxyCountryCode(proxyTable);
             var proxy = "";
 
             foreach (var nw in ip.Zip(ports, Tuple.Create))
@@ -33,6 +36,10 @@ namespace ProxySharp
                 proxyQueue.Add(proxy);
             }
 
+            if (countryCode != null && codes.Contains(countryCode))
+            {
+                proxyQueue = FilterProxy(proxyQueue, codes, countryCode, excludeCountry);
+            }
             return proxyQueue;
         }
 
@@ -74,5 +81,67 @@ namespace ProxySharp
             return proxyPorts;
         }
 
+        /// <summary>
+        /// Gets all of the Proxy Country Codes from the scraped proxy table.
+        /// </summary>
+        /// <param name="proxyTable">The scraped table that holds all of the proxy information.</param>
+        /// <returns>A list of country codes.</returns>
+        private static List<string> ScrapingProxyCountryCode(HtmlNode proxyTable)
+        {
+            var proxyCode = new List<string>();
+
+            var allCodes = proxyTable.SelectNodes("tbody/tr/td[3]");
+
+            foreach (var code in allCodes)
+            {
+                proxyCode.Add(code.InnerHtml);
+            }
+
+            return proxyCode;
+        }
+
+        /// <summary>
+        /// Filter the proxies by country code. Remove all proxies that do not match the filter.
+        /// </summary>
+        /// <param name="proxyQueue">The list of gathered proxies.</param>
+        /// <param name="codes">The list of gathered country codes.</param>
+        /// <param name="countryCode">The two-letter code of the country to filter by (e.g., "US", "JP", "NZ").</param>
+        /// <param name="excludeCountry">Set to true to exclude all proxies that match the filter; set to false to include only proxies that match the filter.</param>
+        /// <returns>A list of filtered proxies</returns>
+        private static List<string> FilterProxy(List<string> proxyQueue, List<string> codes, string countryCode, bool excludeCountry)
+        {
+            List<int> indexes = new List<int>();
+
+            switch (excludeCountry)
+            {
+                case false:
+                    for (int i = 0; i < codes.Count; i++)
+                    {
+                        if (codes[i] != countryCode)
+                        {
+                            indexes.Add(i);
+                        }
+                    }
+                    break;
+                case true:
+                    for (int i = 0; i < codes.Count; i++)
+                    {
+                        if (codes[i] == countryCode)
+                        {
+                            indexes.Add(i);
+                        }
+                    }
+                    break;
+            }
+
+            indexes.Reverse(); // Needed to avoids index out of range errors or non-targeted proxies deletions.
+
+            foreach (var index in indexes)
+            {
+                proxyQueue.RemoveAt(index);
+            }
+
+            return proxyQueue;
+        }
     }
 }
