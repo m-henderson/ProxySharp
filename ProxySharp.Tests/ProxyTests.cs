ec50e25fc1a1350badc55121c832d9de102775ca
Miiraak <miiraak@feynlok.ch>
2024-08-20 18:45:13 +0200

    UI

---
+++ ProxySharp.Tests/ProxyTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace ActionTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, 1);    
+        }
+    }
+}
